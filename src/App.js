d2cd1afd5dc34e8973b0e682aeaebb7c27a97482
Joey Baker <jbaker@matchsquare.com>
2019-11-25 23:05:52 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
+import fetch from 'node-fetch';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { createHttpLink } from 'apollo-link-http';
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
 import './App.css';
 
-function App() {
+const PING = gql`
+  query {ping}
+`;
+
+const client = new ApolloClient({
+  link: createHttpLink({
+    uri: "http://code-test.matchsquare.com/graphql-beta",
+    fetch: fetch
+  }),
+  cache: new InMemoryCache()
+});
+
+const Content = () => {
+  const {
+    data: { ping = "" } = {},
+    error,
+    loading
+  } = useQuery(PING, {});
+
+  if(error){
+    return (
+      <p>{error}</p>
+    )
+  }
+
+  if(loading){
+    return (
+      <p>{loading}</p>
+    )
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>ping: {ping}</p>
       </header>
     </div>
   );
 }
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <Content />
+  </ApolloProvider>
+)
 
 export default App;
